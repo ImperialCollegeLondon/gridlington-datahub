8323d10eb4b9be1dce9034fdf187e5480917adb4
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2023-05-11 17:22:50 +0100

    Comments to explain pandas data munging

--- datahub/wesim.py
+++ datahub/wesim.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 REGIONS_KEY = {
     "Scotland": "SCO",
-    "North Engl&Wal": "NEW",
+    "North Eng&Wal": "NEW",
     "Midlands": "MID",
     "London": "LON",
     "South Eng&Wal": "SEW",
@@ -26,6 +26,7 @@ def read_wesim() -> dict[int | str, pd.DataFrame]:
         index_col=1,
     )
 
+    # Remove Sheet1 (it is just the totals from all the other sheets)
     excel.pop("Sheet1", None)
 
     return excel
@@ -47,6 +48,7 @@ def structure_wesim(df: pd.DataFrame) -> pd.DataFrame:
     """
     df = df.dropna(axis="columns", how="all")
 
+    # Select only the columns with Regions, Interconnectors or Total data
     df = df[
         [
             col
@@ -55,8 +57,11 @@ def structure_wesim(df: pd.DataFrame) -> pd.DataFrame:
             in set(REGIONS_KEY.values()).union(INTERCONNECTORS_KEY).union({"Total"})
         ]
     ]
+
+    # Change from multi-level columns headings to vertically-stacked columns
     stacked = df.stack()
 
+    # Move the Hour and Region Code data from a multi-index to individual columns
     if isinstance(stacked, pd.DataFrame):
         df = stacked.reset_index(names=["Hour", "Region Code"])
         df.columns.name = None
@@ -78,11 +83,15 @@ def structure_capacity(df: pd.DataFrame) -> pd.DataFrame:
         A structured Dataframe
     """
     df = df.dropna(axis="columns", how="all")
+
+    # Select only the columns within the Region table and make Technologies the columns
     df = df[[col for col in df.columns if col[0] == "Region"]]
     df = df.transpose()
     df.index = df.index.droplevel(0)
-    df.index.name = "Region"
-    return df.reset_index().replace({"Region": REGIONS_KEY})
+    df.index.name = "Region Code"
+
+    # Return with the a Region columns matching the regions keys
+    return df.reset_index().replace({"Region Code": REGIONS_KEY})
 
 
 def structure_interconnectors(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
@@ -97,13 +106,14 @@ def structure_interconnectors(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFr
         Two structured Dataframes
     """
     df = df.dropna(axis="columns", how="all")
-    interconnectors = structure_wesim(df[df.columns[:5]])
 
+    # Separate out interconnector flow data from the capacity data
+    interconnectors = structure_wesim(df[df.columns[:5]])
     interconnector_capacity = df[["Code", "Capacity (MW)"]].dropna().T.reset_index().T
-    interconnector_capacity.rename(
-        columns=dict(
-            zip(interconnector_capacity.columns, interconnector_capacity.iloc[0])
-        )
+
+    # Name the columns corerctly and select the correct rows
+    interconnector_capacity = interconnector_capacity.rename(
+        columns=dict(zip(interconnector_capacity.columns, ["Region Code", "Capacity"]))
     )
     interconnector_capacity = interconnector_capacity.iloc[1:].reset_index(drop=True)
 
@@ -124,6 +134,7 @@ def get_wesim() -> dict[str, pd.DataFrame]:
         excel.pop("Interconnector flows")
     )
 
+    # Combine the regions data from separate sheets into one DF
     regions = pd.DataFrame(columns=["Hour", "Region Code"])
     for df in excel.values():
         regions = regions.merge(structure_wesim(df), how="outer")
