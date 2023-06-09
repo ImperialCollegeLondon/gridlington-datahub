fdd7ee0f1cc13bdb9b496f617529875ba64b851f
CWestICL <cwest@ic.ac.uk>
2023-06-09 10:18:09 +0100

    Global variable reset fixture

--- tests/test_opal_api.py
+++ tests/test_opal_api.py
@@ -1,13 +1,21 @@
 import json
 
 import pandas as pd
+import pytest
 from fastapi.testclient import TestClient
 
 from datahub import data as dt
 from datahub.main import app
+from datahub.opal import create_opal_frame
 
 client = TestClient(app)
 client.headers["Content-Type"] = "application/json"
+
+
+@pytest.fixture(autouse=True)
+def reset_opal_data():
+    """Pytest Fixture for resetting Opal data global variable."""
+    dt.opal_df = create_opal_frame()
 
 
 def test_post_opal_api(opal_data):
@@ -28,13 +36,19 @@ def test_post_opal_api(opal_data):
 
 
 def test_post_opal_api_array(opal_data_array):
-    """Tests POSTing Opal data to API."""
+    """Tests POSTing Opal data to API in array format."""
     post_data = json.dumps({"array": opal_data_array.copy()})
+
+    """Checks that the Opal global variable has been reset."""
+    assert len(dt.opal_df.index) == 1
 
     """Checks that a POST request can be successfully made."""
     response = client.post("/opal", data=post_data)
     assert response.status_code == 200
     assert response.json() == {"message": "Data submitted successfully."}
+
+    """Checks that the Opal global variable has been updated."""
+    assert len(dt.opal_df.index) == 2
 
 
 def test_post_opal_api_invalid(opal_data, opal_data_array):
@@ -55,8 +69,11 @@ def test_post_opal_api_invalid(opal_data, opal_data_array):
     assert response.status_code == 400
 
 
-def test_get_opal_api():
+def test_get_opal_api(opal_data):
     """Tests Opal data GET method."""
+    post_data = json.dumps(opal_data.copy())
+    client.post("/opal", data=post_data)
+
     response = client.get("/opal")
     get_data = pd.DataFrame.from_dict(response.json())
 
