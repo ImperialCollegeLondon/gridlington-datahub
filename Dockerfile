4f99867ee236e9184a3ad2fa385b3507119174d8
CWestICL <cwest@ic.ac.uk>
2023-07-24 14:45:31 +0100

    Uvicorn logs to file

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ RUN pip install --no-cache-dir --upgrade -r /src/requirements.txt
 
 COPY ./datahub /src/app
 
+COPY .run.py /src/run.py
+
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- datahub/core/log_config.py
+++ datahub/core/log_config.py
@@ -6,12 +6,22 @@ FORMAT: str = "[%(levelname)s] %(asctime)s | %(message)s"
 logging_dict_config = {
     "version": 1,
     "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(message)s",
+            "use_colors": "None",
+        },
         "basic": {
             "()": "uvicorn.logging.DefaultFormatter",
             "format": FORMAT,
-        }
+        },
     },
     "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
         "console": {
             "formatter": "basic",
             "class": "logging.StreamHandler",
@@ -26,6 +36,7 @@ logging_dict_config = {
         },
     },
     "loggers": {
+        "uvicorn": {"handlers": ["default", "file"], "level": "INFO"},
         "api_logger": {
             "handlers": ["console", "file"],
             "level": LOG_LEVEL,
